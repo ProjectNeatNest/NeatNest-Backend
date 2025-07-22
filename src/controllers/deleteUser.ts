376f04606f59f6fa82e2761458f3649e47e0e1fa
Nastya Rosa <nastyaagb@gmail.com>
2025-07-22 13:25:04 +0200

    created controllers and updated routes

---
+++ src/controllers/deleteUser.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+
+import { sendQuery } from '../config/db/dbConfig.js';
+import HTTPError from '../models/HTTPError.js';
+
+export default async function deleteUser(req: Request, res: Response) {
+    const { user_id } = req.params;
+
+    const [userFound] = await sendQuery(
+        'SELECT * FROM users WHERE user_id = $1',
+        [user_id]
+    );
+
+    if (!userFound) throw new HTTPError(404, 'User not found.');
+
+    const [deletedUser] = await sendQuery(
+        'DELETE FROM users WHERE user_id = $1 RETURNING *',
+        [user_id]
+    );
+
+    res.status(200).send({ message: 'User deleted', data: deletedUser });
+}
