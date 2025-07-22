fdd7d680889812eb8c6336c78a337e7f2e3d2a07
Nastya Rosa <nastyaagb@gmail.com>
2025-07-22 13:25:42 +0200

    created controllers and updated routes

---
+++ src/controllers/getAllUsers.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express';
+
+import { sendQuery } from '../config/db/dbConfig.js';
+
+export default async function getAllUsers(req: Request, res: Response) {
+    const users = await sendQuery('SELECT * FROM users');
+    res.send({ message: 'Users list', data: users });
+}
---
+++ src/controllers/loginUser.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import { sendQuery } from '../config/db/dbConfig.js';
+import { loginSchema } from '../schemas/userSchemas.js';
+import HTTPError from '../models/HTTPError.js';
+
+export default async function loginUser(req: Request, res: Response) {
+    const { email, password } = loginSchema.parse(req.body);
+
+    const [userFound] = await sendQuery(
+        'SELECT * FROM users WHERE email = $1',
+        [email]
+    );
+
+    if (!userFound) throw new HTTPError(400, 'Incorrect User and/or password.');
+
+    const passwordMatch = bcrypt.compareSync(password, userFound.password);
+
+    if (!passwordMatch)
+        throw new HTTPError(400, 'Incorrect User and/or password.');
+
+    const tokenData = {
+        user_id: userFound.user_id,
+        username: userFound.username,
+        email: userFound.email,
+    };
+
+    const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+        expiresIn: '30 days',
+    });
+
+    res.send({ message: 'User logged in', data: token });
+}
