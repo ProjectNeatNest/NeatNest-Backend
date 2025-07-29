af015b598999f4e4853fae107787b14ff59bc512
Nastya Rosa <nastyaagb@gmail.com>
2025-07-29 13:43:15 +0200

    updated getAllAreas

--- src/controllers/areas/getAllAreas.ts
+++ src/controllers/areas/getAllAreas.ts
@@ -1,8 +1,24 @@
 import { Request, Response } from 'express';
 
 import { sendQuery } from '../../config/db/dbConfig.js';
+import { AuthorizedRequest } from '../../config/types.js';
 
 export default async function getAllAreas(req: Request, res: Response) {
-    const areas = await sendQuery('SELECT * FROM areas');
+    const { user_id } = (req as AuthorizedRequest).user;
+
+    const { housing_id } = req.body;
+
+
+    const housingUsersIds = await sendQuery('SELECT user_id FROM users_housings WHERE housing_id = $1'); 
+
+    console.log(housingUsersIds);
+
+    
+
+    const areas = await sendQuery(
+        'SELECT name FROM areas WHERE housing_id = $1',
+        [housing_id]
+    );
+
     res.send({ message: 'Areas list', data: areas });
 }
