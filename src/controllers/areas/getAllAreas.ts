2c20684abd84b50188fb3eace3de821bdc7a694c
Nastya Rosa <nastyaagb@gmail.com>
2025-07-30 09:44:04 +0200

    updated getAllAreas

--- src/controllers/areas/getAllAreas.ts
+++ src/controllers/areas/getAllAreas.ts
@@ -6,14 +6,11 @@ import { AuthorizedRequest } from '../../config/types.js';
 export default async function getAllAreas(req: Request, res: Response) {
     const { user_id } = (req as AuthorizedRequest).user;
 
-    const { housing_id } = req.body;
-
+    const { housing_id } = req.params;
 
     const housingUsersIds = await sendQuery('SELECT user_id FROM users_housings WHERE housing_id = $1'); 
 
     console.log(housingUsersIds);
-
-    
 
     const areas = await sendQuery(
         'SELECT name FROM areas WHERE housing_id = $1',
