2744d13bcb81dcd552511343e2a42283f7933497
Andrea <andreagoal96@gmail.com>
2025-07-30 10:55:29 +0200

    fixed issue with param

--- src/controllers/areas/getAllAreas.ts
+++ src/controllers/areas/getAllAreas.ts
@@ -1,19 +1,13 @@
 import { Request, Response } from 'express';
 
 import { sendQuery } from '../../config/db/dbConfig.js';
-import { AuthorizedRequest } from '../../config/types.js';
 
 export default async function getAllAreas(req: Request, res: Response) {
-    const { user_id } = (req as AuthorizedRequest).user;
 
     const { housing_id } = req.params;
 
-    const housingUsersIds = await sendQuery('SELECT user_id FROM users_housings WHERE housing_id = $1'); 
-
-    console.log(housingUsersIds);
-
     const areas = await sendQuery(
-        'SELECT name FROM areas WHERE housing_id = $1',
+        'SELECT * FROM areas WHERE housing_id = $1',
         [housing_id]
     );
 
--- src/controllers/housings/getAllHousings.ts
+++ src/controllers/housings/getAllHousings.ts
@@ -6,11 +6,9 @@ import { AuthorizedRequest } from '../../config/types.js';
 export default async function getAllHousings(req: Request, res: Response) {
     const { user_id } = (req as AuthorizedRequest).user;
 
-
     const housingUsersIds = await sendQuery('SELECT housing_id FROM users_housings WHERE user_id = $1', [user_id]);
-
     const ids = housingUsersIds.map(housing => housing.housing_id);
-
+    
     const housings = await sendQuery(
         'SELECT * FROM housings WHERE housing_id = ANY($1)',
         [ids]
