6b2f418a8467c1e63c3936eb8a6a96f68954521b
Nastya Rosa <nastyaagb@gmail.com>
2025-07-31 10:04:28 +0200

    updated housings, tasks and housingsAreas routes

--- src/routes/housingsAreas.routes.ts
+++ src/routes/housingsAreas.routes.ts
@@ -5,6 +5,7 @@ import createArea from '../controllers/areas/createArea.js';
 import updateArea from '../controllers/areas/updateArea.js';
 import deleteArea from '../controllers/areas/deleteArea.js';
 import getOneArea from '../controllers/areas/getOneArea.js';
+import { tasksRouter } from './tasks.routes.js';
 
 export const areasRouter = express.Router({ mergeParams: true});
 
@@ -18,3 +19,6 @@ areasRouter.post('/', createArea);
 areasRouter.get('/:area_id', getOneArea);
 areasRouter.patch('/:area_id', updateArea);
 areasRouter.delete('/:area_id', deleteArea);
+
+// Routing to /housings/:housing_id/areas/:area_id
+areasRouter.use('/:area_id/tasks', tasksRouter);
