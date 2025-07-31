c2046bfe16a5b93e5b07550c3003f2b3f770821d
Nastya Rosa <nastyaagb@gmail.com>
2025-07-31 10:59:04 +0200

    updated getOneTask

--- src/controllers/tasks/getOneTask.ts
+++ src/controllers/tasks/getOneTask.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from 'express';
 
 import { sendQuery } from '../../config/db/dbConfig.js';
+import HTTPError from '../../models/HTTPError.js';
 
 export default async function getOneTask(req: Request, res: Response) {
-   const { task_id, area_id } = req.body; 
+   const { task_id, area_id } = req.params; 
 
-   const task = await sendQuery(
+   const [task] = await sendQuery(
       'SELECT name FROM tasks WHERE task_id = $1 AND area_id = $2',
       [task_id, area_id]
    );
+
+   if (!task) throw new HTTPError(404, 'Task not found');
 
    res.send({ message: 'Task details', data: task})
 }
