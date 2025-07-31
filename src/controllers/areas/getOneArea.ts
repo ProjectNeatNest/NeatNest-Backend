127dcb226b3e736d55651d6ef45ccfb7e55c0b16
Nastya Rosa <nastyaagb@gmail.com>
2025-07-31 10:52:22 +0200

    updated getOneArea

--- src/controllers/areas/getOneArea.ts
+++ src/controllers/areas/getOneArea.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from 'express';
 
 import { sendQuery } from '../../config/db/dbConfig.js';
+import HTTPError from '../../models/HTTPError.js';
 
 export default async function getOneArea(req: Request, res: Response) {
    const { housing_id, area_id } = req.params; 
 
-   const area = await sendQuery(
+   const [area] = await sendQuery(
       'SELECT * FROM areas WHERE housing_id = $1 AND area_id = $2',
       [housing_id, area_id]
    );
+
+   if (!area) throw new HTTPError(404, 'Area not found');
 
    res.send({ message: 'Area details', data: area})
 }
