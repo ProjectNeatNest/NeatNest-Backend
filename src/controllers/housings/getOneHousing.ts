af54666a5676d63ff935a9e810db0139b43c9af0
Nastya Rosa <nastyaagb@gmail.com>
2025-07-31 10:58:25 +0200

    updated getOneHousing

--- src/controllers/housings/getOneHousing.ts
+++ src/controllers/housings/getOneHousing.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from 'express';
 
 import { sendQuery } from '../../config/db/dbConfig.js';
+import HTTPError from '../../models/HTTPError.js';
 
 export default async function getOneHousing(req: Request, res: Response) {
-   const { housing_id } = req.body; 
+   const { housing_id } = req.params; 
 
-   const housing = await sendQuery(
+   const [housing] = await sendQuery(
       'SELECT name FROM housings WHERE housing_id = $1',
       [housing_id]
    );
+
+   if (!housing) throw new HTTPError(404, 'Housing not found');
 
    res.send({ message: 'Housing details', data: housing})
 }
