21beb90710f77476dd3f6824cd5f4ff68a858b9d
Nastya Rosa <nastyaagb@gmail.com>
2025-07-31 10:38:18 +0200

    updated getOneArea

--- src/controllers/areas/getOneArea.ts
+++ src/controllers/areas/getOneArea.ts
@@ -6,7 +6,7 @@ export default async function getOneArea(req: Request, res: Response) {
    const { housing_id, area_id } = req.params; 
 
    const area = await sendQuery(
-      'SELECT name FROM areas WHERE housing_id = $1 AND area_id = $2',
+      'SELECT * FROM areas WHERE housing_id = $1 AND area_id = $2',
       [housing_id, area_id]
    );
 
