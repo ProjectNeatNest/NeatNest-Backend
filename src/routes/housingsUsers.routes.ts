526f1bf89cd1694e0021835d16eecfefe5e2ee3d
Nastya Rosa <nastyaagb@gmail.com>
2025-07-31 09:53:53 +0200

    added adminAuth to deleteUsersHousings

--- src/routes/housingsUsers.routes.ts
+++ src/routes/housingsUsers.routes.ts
@@ -4,11 +4,13 @@ import getAllUsersHousings from '../controllers/usersHousings/getAllUsersHousing
 import createUsersHousings from '../controllers/usersHousings/createUsersHousings.js';
 import updateUsersHousings from '../controllers/usersHousings/updateUsersHousings.js';
 import deleteUsersHousings from '../controllers/usersHousings/deleteUsersHousings.js';
+
 import { userAuth } from '../middlewares/userAuth.js';
+import { adminAuth } from '../middlewares/adminAuth.js';
 
 export const housingsUsersRouter = express.Router({ mergeParams: true });;
 
 housingsUsersRouter.get('/', userAuth, getAllUsersHousings);
 housingsUsersRouter.post('/', userAuth, createUsersHousings);
 housingsUsersRouter.patch('/:user_id', userAuth, updateUsersHousings);
-housingsUsersRouter.delete('/:user_id', userAuth, deleteUsersHousings);
+housingsUsersRouter.delete('/:user_id', userAuth, adminAuth, deleteUsersHousings);
