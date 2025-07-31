04dffd537942a5fe22218a4fc05800e41c0d2890
Nastya Rosa <nastyaagb@gmail.com>
2025-07-31 12:21:18 +0200

    edited getAllUsersHousings

--- src/controllers/usersHousings/getAllUsersHousings.ts
+++ src/controllers/usersHousings/getAllUsersHousings.ts
@@ -4,11 +4,15 @@ import { sendQuery } from '../../config/db/dbConfig.js';
 
 export default async function getAllUsersHousings(req: Request, res: Response) {
     const { housing_id } = req.params;
+
     const userHousings = await sendQuery(
         'SELECT * FROM users_housings WHERE housing_id = $1',
         [housing_id]
     );
-    const userIds = userHousings.map(userHousing => userHousing.user_id)
+
+    const userIds = userHousings.map(userHousing => userHousing.user_id);
+
     const users = await sendQuery('SELECT * FROM users WHERE user_id IN (' + userIds.join(',') + ')');
+
     res.send({ message: 'Users in housing list', data: users });
 }
