617e9a09cc243cbd55b1022b69d3e0df50136878
Nastya Rosa <nastyaagb@gmail.com>
2025-08-02 10:46:36 +0200

    updated getAllTasksFromUser

--- src/controllers/tasks/getAllTasksFromUser.ts
+++ src/controllers/tasks/getAllTasksFromUser.ts
@@ -5,10 +5,10 @@ import { sendQuery } from '../../config/db/dbConfig.js';
 export default async function getAllTasksFromUsers(req: Request, res: Response) {
     const { user_id, area_id } = req.params;
 
-    const tasks = await sendQuery(
+    const userTasks = await sendQuery(
         'SELECT * FROM tasks WHERE user_id = $1 AND area_id = $2',
         [user_id, area_id]
     );
 
-    res.send({ message: 'User tasks list', data: tasks });
+    res.send({ message: 'User tasks list', data: userTasks });
 }
