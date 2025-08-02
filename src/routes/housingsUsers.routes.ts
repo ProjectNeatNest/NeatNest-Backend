232e9edbbefab114309461c8690acfe05b6787ce
Nastya Rosa <nastyaagb@gmail.com>
2025-08-02 11:50:42 +0200

    updated housingsUsers routes by adding the getAllTasksFromUser

--- src/routes/housingsUsers.routes.ts
+++ src/routes/housingsUsers.routes.ts
@@ -4,6 +4,7 @@ import getAllUsersHousings from '../controllers/usersHousings/getAllUsersHousing
 import createUsersHousings from '../controllers/usersHousings/createUsersHousings.js';
 import updateUsersHousings from '../controllers/usersHousings/updateUsersHousings.js';
 import deleteUsersHousings from '../controllers/usersHousings/deleteUsersHousings.js';
+import getAllTasksFromUser from '@/controllers/usersHousings/getAllTasksFromUser.js';
 
 import { adminAuth } from '../middlewares/adminAuth.js';
 
@@ -13,3 +14,4 @@ housingsUsersRouter.get('/', getAllUsersHousings);
 housingsUsersRouter.post('/', createUsersHousings);
 housingsUsersRouter.patch('/:user_id', updateUsersHousings);
 housingsUsersRouter.delete('/:user_id', adminAuth, deleteUsersHousings);
+housingsUsersRouter.get('/:user_id/tasks', getAllTasksFromUser);
