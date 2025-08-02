6f4454bf764b4acff3952685a513dc7fd2b1e672
Nastya Rosa <nastyaagb@gmail.com>
2025-08-02 11:56:20 +0200

    updated housingsUsers routes by adding the getAllTasksFromUser

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -5,12 +5,10 @@ import createTask from '../controllers/tasks/createTasks.js';
 import updateTask from '../controllers/tasks/updateTask.js';
 import deleteTask from '../controllers/tasks/deleteTask.js';
 import getOneTask from '@/controllers/tasks/getOneTask.js';
-import getAllTasksFromUser from '@/controllers/tasks/getAllTasksFromUser.js';
 
 export const tasksRouter = express.Router({ mergeParams: true });
 
 tasksRouter.get('/', getAllTasksFromArea);
-tasksRouter.get('/', getAllTasksFromUser)
 tasksRouter.post('/', createTask);
 tasksRouter.get('/:task_id', getOneTask);
 tasksRouter.patch('/:task_id', updateTask);
